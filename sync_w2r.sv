e2e944e2fbb9774b71ac5dae0e2e530169da806d
Raghava Bathula <56624478+RaghavaBathula@users.noreply.github.com>
2024-02-03 16:15:27 -0800

    Update sync_w2r.sv

--- sync_w2r.sv
+++ sync_w2r.sv
@@ -1,15 +1,4 @@
-/*
-6.4 sync_w2r.v - Write-domain to read-domain synchronizer
-This is a simple synchronizer module, used to pass an n-bit pointer from the write clock domain to the read clock
-domain, through a pair of registers that are clocked by the FIFO read clock. Notice the simplicity of the always
-block that concatenates the two registers together for reset and shifting. The synchronizer always block is only three
-lines of code.
-All module outputs are registered for simplified synthesis using time budgeting. All outputs of this module are
-entirely synchronous to the rclk and all asynchronous inputs to this module are from the wclk domain with all
-signals named using an “w” prefix, making it easy to set a false path on all “w*” signals for simplified static timing
-analysis.
-*/
-
+//sync_w2r.sv
 module sync_w2r #(parameter ADDRSIZE = 9)
  (output reg [ADDRSIZE:0] rq2_wptr,
  input [ADDRSIZE:0] wptr,
