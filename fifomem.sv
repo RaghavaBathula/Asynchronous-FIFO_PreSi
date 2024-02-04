b91a861b084c06cc7e665f6582307834545d8f6b
Bhavana Maddu <117325575+bhavanamaddu2308@users.noreply.github.com>
2024-02-03 19:45:26 -0800

    Update fifomem.sv

--- fifomem.sv
+++ fifomem.sv
@@ -6,19 +6,13 @@ module fifomem #(parameter DATASIZE = 32, // Memory data word width
  input [ADDRSIZE-1:0] waddr, raddr,
  input wclken, wfull, wclk);
  
- `ifdef VENDORRAM
- // instantiation of a vendor's dual-port RAM
- vendor_ram mem (.dout(rdata), .din(wdata),
- .waddr(waddr), .raddr(raddr),
- .wclken(wclken),
- .wclken_n(wfull), .clk(wclk));
- `else
+ 
  // RTL Verilog memory model
  localparam DEPTH = 1<<ADDRSIZE;    //2^^9?
  reg [DATASIZE-1:0] mem [0:DEPTH-1];
  assign rdata = mem[raddr];            //Shouldn't we check the empty codn here?
  always @(posedge wclk)
  if (wclken && !wfull) mem[waddr] <= wdata;   //why wclken?
- `endif
+
  
 endmodule
